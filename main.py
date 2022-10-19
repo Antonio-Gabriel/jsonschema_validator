fdfc283f650d89b17c33583874defa8f8396b2bd
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-10-19 22:09:25 +0100

    refactor: remove schema on this local

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 from jsonschema import validate
 
-schema = {
-    "type": "object",
-    "properties": {
-        "name": {
-            "type": "string"
-        },
-        "email": {
-            "type": "string",
-            "pattern": "^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"
-        },
-        "phone": {
-            "type": "number"
-        }
-    }
-}
+from src.validator.schema import schema
 
 try:
     validate(instance={
